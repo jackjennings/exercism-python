ab956d5176dacee0dd93b2dfad358a4bec0215f2
Jack Jennings <jack@standard-library.com>
2018-12-27 15:20:49 -0800

    book-store: solution

---
+++ book-store/book_store.py
@@ -0,0 +1,24 @@
+from collections import Counter
+
+DISCOUNTS = {
+    0: 0,
+    1: 0,
+    2: 0.05,
+    3: 0.10,
+    4: 0.20,
+    5: 0.25
+}
+
+def calculate_total(books):
+    counts = Counter(books)
+    total = 0
+
+    while counts:
+        remaining = set(counts.elements())
+        discount = DISCOUNTS[len(remaining)]
+        subtotal = len(remaining) * 800
+
+        total += subtotal - int(subtotal * discount)
+        counts -= Counter(remaining)
+
+    return total
