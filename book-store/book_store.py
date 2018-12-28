0ebbd2e7e7707d1c2a75f2f3654c3da2d9350137
Jack Jennings <jack@standard-library.com>
2018-12-27 16:40:12 -0800

    book-store: reformat

--- book-store/book_store.py
+++ book-store/book_store.py
@@ -1,13 +1,7 @@
 from collections import Counter
 
-DISCOUNTS = {
-    0: 0,
-    1: 0,
-    2: 0.05,
-    3: 0.10,
-    4: 0.20,
-    5: 0.25
-}
+DISCOUNTS = {0: 0, 1: 0, 2: 0.05, 3: 0.10, 4: 0.20, 5: 0.25}
+
 
 def calculate_total(books):
     counts = Counter(books)
