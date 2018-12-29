e9ec549fb0583ea87b92ad33493353ac414ccf69
Jack Jennings <jack@standard-library.com>
2018-12-29 12:43:44 -0800

    etl: Add type hints

--- etl/etl.py
+++ etl/etl.py
@@ -1,4 +1,7 @@
-def transform(legacy_data):
+from typing import Dict, List
+
+
+def transform(legacy_data: Dict[int, List[str]]) -> Dict[str, int]:
     output = {}
 
     for score, letters in legacy_data.items():
